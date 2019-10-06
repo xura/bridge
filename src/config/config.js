9a15c1feb46aa667f02eda48479550eeb4db6028
Joe Badaczewski <joe307bad@gmail.com>
2019-10-06 00:20:04 -0400

    building bridge project

--- src/config/config.js
+++ src/config/config.js
@@ -1,26 +1,13 @@
 import { registerApplication, start } from 'single-spa'
 import { connect, data } from '@xura/data';
 
-// @ts-ignore
-window.emp = await connect()
-    .then(connection => data(connection))
+connect().then(connection => {
+    registerApplication(
+        'home',
+        () => SystemJS.import('@xura/feed'),
+        () => true
+    );
 
-registerApplication(
-    // Name of our single-spa application
-    'home',
-    // loadingFunction
-    () => SystemJS.import('@xura/feed'),
-    // activityFunction
-    () => true
-);
-
-// registerApplication(
-//     // Name of our single-spa application
-//     'navBar',
-//     // loadingFunction
-//     () => import('./src/navBar.app.js'),
-//     // activityFunction
-//     () => true
-// );
-
-start();
+    start();
+    return data(connection);
+});
