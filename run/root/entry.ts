cc20532ed7e2830754d634fa2f9cec557e2b3716
Joe Badaczewski <joe307bad@gmail.com>
2020-01-26 22:53:08 -0500

    working with form builder

--- run/root/entry.ts
+++ run/root/entry.ts
@@ -5,6 +5,8 @@ import { connect, data } from '@xura/data';
 connect().then(_ => {
     // @ts-ignore
     window.d = data;
+    // @ts-ignore
+    data.achievements.form('insert-here')
     registerApplication(
         'home',
         // @ts-ignore
