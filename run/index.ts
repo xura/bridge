a7b21fa2704bec29e57473c6de8f3001604f3ed3
Joe Badaczewski <joe307bad@gmail.com>
2020-04-06 19:52:59 -0400

    adding commands for running mason/nestjs and building gql types for emporium from mason

--- run/index.ts
+++ run/index.ts
@@ -1,9 +1,22 @@
 import concurrently from 'concurrently';
 import registry from './registry';
+import path from 'path'
+import { exec } from 'shelljs';
+
+const relPath = (file: string) => path.resolve(__dirname, file);
 
 const wdsCommands = registry.map(project => ({
     command: `${project[1]} ${project[2]}`.toString() || '',
     name: project[0]?.toString() || ''
 }));
 
-concurrently(wdsCommands);
+const mason = {
+    command: `yarn --cwd ${relPath('../../mason')} start`,
+    name: "mason"
+};
+
+new Promise(resolve => {
+    exec(`generate-graphql-client -s ${relPath("../../mason/schema.gql")} -o ${relPath("../../emporium/mason")}`, { async: true }, () => resolve());
+}).then(() =>
+    concurrently([...wdsCommands, mason])
+);
